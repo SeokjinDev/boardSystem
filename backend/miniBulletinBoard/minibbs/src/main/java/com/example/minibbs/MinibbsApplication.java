b155ee002e511cdf7d51eec370ab9b1932ea6ee5
SeokjinDev <tjrwls1108011@gmail.com>
2024-07-10 04:26:25 +0900

    Init miniBulletinBoard Backend

---
+++ backend/miniBulletinBoard/minibbs/src/main/java/com/example/minibbs/MinibbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.minibbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinibbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinibbsApplication.class, args);
+	}
+
+}
---
+++ backend/miniBulletinBoard/minibbs/src/test/java/com/example/minibbs/MinibbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minibbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinibbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
