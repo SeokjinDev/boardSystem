2a3cb24598b5ab8c611da208760b22412ac9379f
SeokjinDev <tjrwls1108011@gmail.com>
2024-07-10 04:52:10 +0900

    Feat: Link Database Link To Posts Table In minibbs

---
+++ backend/miniBulletinBoard/minibbs/src/main/java/com/example/minibbs/entity/Post.java
@@ -0,0 +1,56 @@
+package com.example.minibbs.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+    
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "pw")
+    private String password;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    
+    @Builder
+    public Post(String title, String content, String author, String password) {
+        this.title = title;
+        this.content = content;
+        this.author = content;
+        this.password = password;
+    }
+}
